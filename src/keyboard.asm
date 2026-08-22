fbc3ff92ec69ce367f1f81fca8ffae462a872cec
Szymon Wilczek <swilczek.lx@gmail.com>
2026-08-22 23:00:39 +0200

    keyboard: functional backspace key
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

--- src/keyboard.asm
+++ src/keyboard.asm
@@ -1,12 +1,31 @@
+xor cx, cx ; counter=0
+
 read_char:
 	mov ah, 0x00
 	int 0x16
-	call print_char
+
 	cmp al, 0x0D
-	jne read_char
 	je .enter
+	cmp al, 0x08
+	je .backspace
+
+	call print_char
+	inc cx
+	jmp read_char
 
 	.enter:
 		call print_enter
+		xor cx, cx
 		jmp read_char
 
+	.backspace:
+		cmp cx, 0
+		je read_char
+		mov al, 0x08
+		call print_char
+		mov al, 0x20
+		call print_char
+		mov al, 0x08
+		call print_char
+		dec cx
+		jmp read_char
