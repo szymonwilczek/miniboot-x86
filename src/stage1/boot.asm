0e87276ac87df0625c07a563efa414277f2401ff
Szymon Wilczek <swilczek.lx@gmail.com>
2026-08-23 00:56:24 +0200

    boot: stage 1 boot controller
    I'm very proud of him. He's trying his best.
    
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

---
+++ src/stage1/boot.asm
@@ -0,0 +1,39 @@
+[bits 16]
+[org 0x7C00]
+
+boot:
+	mov [BOOT_DRIVE], dl
+	xor ax, ax
+	mov ds, ax
+	mov es, ax ; target sector in RAM
+	mov ss, ax
+	mov sp, 0x7C00
+
+	mov ah, 0x02 ; read sectors
+	mov al, 0x04 ; 4 sectors, so thats about 2kB for stage 2
+	mov ch, 0x00 ; cylinder 0
+	mov cl, 0x02 ; start from sector 2
+	mov dh, 0x00 ; start from header 0 (glowica)
+	mov dl, [BOOT_DRIVE] ; read remembered boot drive number
+	mov bx, 0x8000 ; target offset in RAM (physical address)
+	int 0x13
+
+	jc .disk_error
+
+	jmp 0x0000:0x8000
+
+	.disk_error:
+		mov si, error_string
+		call print_string
+		jmp .hang
+	.hang:
+		jmp .hang
+
+BOOT_DRIVE: db 0
+error_string:
+	db "Oh noo. I am very sorry that I failed you... I could not do that.", 0x0D, 0x0A, 0
+
+%include "src/stage2/drivers/print.asm"
+
+times 510 - ($ - $$) db 0
+dw 0xAA55
