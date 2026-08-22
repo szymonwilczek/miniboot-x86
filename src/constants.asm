f292219f9a51580240f10587fa1b2cf16968285b
Szymon Wilczek <swilczek.lx@gmail.com>
2026-08-23 00:15:08 +0200

    const: replace times with equ for cmd_buffer
    And stop wasting bytes in binary file!!!
    
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

--- src/constants.asm
+++ src/constants.asm
@@ -6,6 +6,6 @@ subtitle_string:
 	db "I guess we are doing things kind of modular now", 0x0D, 0x0A, 0
 
 ; INTEGERS
-cmd_buffer: times 32 db 0 ; 32 bajty pustego miejsca na komende
+cmd_buffer: equ 0x7E00
 max_command_chars: equ 32
 
