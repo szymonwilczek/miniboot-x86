8776de2849fb32591ea7a58bacb0280d43e6308c
Szymon Wilczek <swilczek.lx@gmail.com>
2026-08-22 22:13:52 +0200

    const: extract strings to separate file
    Modular... Modular... Modular..
    Why not?
    
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

--- src/boot.asm
+++ src/boot.asm
@@ -18,13 +18,8 @@ start:
 hang:
 	jmp hang
 
-title_string:
-	db "Hello in this funny little bootloader!", 0x0D, 0x0A, 0
-subtitle_string:
-	db "I guess we are doing things kind of modular now", 0x0D, 0x0A, 0
-
 %include "src/print.asm"
-
+%include "src/constants.asm"
 
 ;; END SECTOR
 
---
+++ src/constants.asm
@@ -0,0 +1,5 @@
+title_string:
+	db "Hello in this funny little bootloader!", 0x0D, 0x0A, 0
+subtitle_string:
+	db "I guess we are doing things kind of modular now", 0x0D, 0x0A, 0
+
