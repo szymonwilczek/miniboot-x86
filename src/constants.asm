f0179426c8fbc478385c52248619337d91015152
Szymon Wilczek <swilczek.lx@gmail.com>
2026-08-23 00:10:50 +0200

    const: add int contsts for command buffers
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

--- src/constants.asm
+++ src/constants.asm
@@ -1,5 +1,11 @@
+; STRINGS
+
 title_string:
 	db "Hello in this funny little bootloader!", 0x0D, 0x0A, 0
 subtitle_string:
 	db "I guess we are doing things kind of modular now", 0x0D, 0x0A, 0
+
+; INTEGERS
+cmd_buffer: times 32 db 0 ; 32 bajty pustego miejsca na komende
+max_command_chars: equ 32
 
