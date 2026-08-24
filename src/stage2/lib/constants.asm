187b3d14d10c910eeb047b3c768008aef01ccb98
Szymon Wilczek <swilczek.lx@gmail.com>
2026-08-24 23:16:04 +0200

    const: remove constants master file
    It provided more problems than solutions
    
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

--- src/stage2/lib/constants.asm
+++
@@ -1,5 +0,0 @@
-%include "src/stage2/lib/constants/boot.asm"
-%include "src/stage2/lib/constants/integers.asm"
-%include "src/stage2/lib/constants/menu.asm"
-%include "src/stage2/lib/constants/strings.asm"
-
